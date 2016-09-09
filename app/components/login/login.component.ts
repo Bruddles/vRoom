15b15775a768aaf71979eb478c0ae89805a39f20
Brodie Gilmour <brodie.gilmour@webappuk.com>
2016-09-09 15:38:13 +0100

    Adding code to prevent login if username is empty.

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,19 +1,29 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import {SocketIoService} from './../../services/socket-io.service';
 
-@Component({	
+@Component({
 	moduleId: module.id,
 	selector: 'login',
 	templateUrl: 'login.component.html'
 })
-export class LoginComponent{
+export class LoginComponent {
 	username: string;
 
-	 constructor(private socketIoService: SocketIoService){}
+	constructor(
+		private router: Router,
+		private socketIoService: SocketIoService) { 
+	}
 
-	 userLogin(){
-		debugger;
-	 	this.socketIoService.login(this.username);
-		 //route to room login
-	 }
+	userLogin() {
+		let link = ['/room-login'];
+
+		if (!!this.username && !!this.username.trim()){
+			//route to room login
+			this.router.navigate(link);
+			this.socketIoService.login(this.username);
+		} else {
+			alert('Please enter a username.');
+		}
+	}
 }
